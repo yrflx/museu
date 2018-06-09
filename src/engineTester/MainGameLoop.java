867d382e0b93adc5546045f4b293f651ac2b0dfe
Yuri Félix <yurifelix.yf@gmail.com>
2018-06-09 11:19:53 -0300

    QUADROS POSICIONADOS

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -23,90 +23,10 @@ public class MainGameLoop {
 		Loader loader = new Loader();
 		StaticShader shader = new StaticShader();
 		Renderer renderer = new Renderer(shader);
-		/*
-		float[] vertices = {			
-				-0.5f,0.5f,0,	
-				-0.5f,-0.5f,0,	
-				0.5f,-0.5f,0,	
-				0.5f,0.5f,0,		
-				
-				-0.5f,0.5f,1,	
-				-0.5f,-0.5f,1,	
-				0.5f,-0.5f,1,	
-				0.5f,0.5f,1,
-				
-				0.5f,0.5f,0,	
-				0.5f,-0.5f,0,	
-				0.5f,-0.5f,1,	
-				0.5f,0.5f,1,
-				
-				-0.5f,0.5f,0,	
-				-0.5f,-0.5f,0,	
-				-0.5f,-0.5f,1,	
-				-0.5f,0.5f,1,
-				
-				-0.5f,0.5f,1,
-				-0.5f,0.5f,0,
-				0.5f,0.5f,0,
-				0.5f,0.5f,1,
-				
-				-0.5f,-0.5f,1,
-				-0.5f,-0.5f,0,
-				0.5f,-0.5f,0,
-				0.5f,-0.5f,1
-				
-		};
 		
-		float[] textureCoords = {
-				
-				0,0,
-				0,1,
-				1,1,
-				1,0,			
-				0,0,
-				0,1,
-				1,1,
-				1,0,			
-				0,0,
-				0,1,
-				1,1,
-				1,0,
-				0,0,
-				0,1,
-				1,1,
-				1,0,
-				0,0,
-				0,1,
-				1,1,
-				1,0,
-				0,0,
-				0,1,
-				1,1,
-				1,0
-
-				
-		};
-		
-		int[] indices = {
-				0,1,3,	
-				3,1,2,	
-				4,5,7,
-				7,5,6,
-				8,9,11,
-				11,9,10,
-				12,13,15,
-				15,13,14,	
-				16,17,19,
-				19,17,18,
-				20,21,23,
-				23,21,22
-
-		};*/
-		
-		//RawModel model = loader.loadToVAO(vertices,textureCoords,indices);
 		
 		  
-		//COLOCANDO O chao
+		//COLOCANDO O CHAO
 		RawModel model = OBJloader.loadObjModel("chao", loader);
 				
 		TexturedModel staticModel = new TexturedModel(model,new ModelTexture(loader.loadTexture("textura")));	
@@ -114,7 +34,7 @@ public class MainGameLoop {
 		Entity entity = new Entity(staticModel, new Vector3f(0,-4,-55),0,0,0,1);
 		  
 		
-		//COLOCANDO a parede
+		//COLOCANDO A PAREDE
 		RawModel model2 = OBJloader.loadObjModel("par", loader);
 				
 		TexturedModel staticModel2 = new TexturedModel(model2,new ModelTexture(loader.loadTexture("parede")));	
@@ -122,25 +42,79 @@ public class MainGameLoop {
 		Entity entity2 = new Entity(staticModel2, new Vector3f(0,-4,-55),0,0,0,1);
 		
 		
-		//COLOCANDO O teto
+		//COLOCANDO O TETO
 		RawModel model3 = OBJloader.loadObjModel("chao", loader);
 					
-		TexturedModel staticModel3 = new TexturedModel(model3,new ModelTexture(loader.loadTexture("textura")));	
+		TexturedModel staticModel3 = new TexturedModel(model3,new ModelTexture(loader.loadTexture("teto")));	
 						
 		Entity entity3 = new Entity(staticModel3, new Vector3f(0,7,-55),0,0,0,1);
 		
 		
-		//COLOCANDO A MONALISA
-		RawModel model4 = OBJloader.loadObjModel("monalisa", loader);
 		
-		TexturedModel staticModel4 = new TexturedModel(model4,new ModelTexture(loader.loadTexture("monalisa")));	
+		int alturaQuadros = -4;
+		int primeiraParede = 5;
+		int segundaParede = 25;
+		int terceiraParede = 45;
+		int quartaParede = 65;
 		
-		Entity entity4 = new Entity(staticModel4, new Vector3f(42,-4,-63),0,0,0,1);
+		
+		int esquerdaParede = 0;
+		int direitaParede = 38;
+		
+		
+		/* QUADROS */
+		
+		RawModel model4 = OBJloader.loadObjModel("quadro", loader);
+		
+		
+		
+		
+		//QUADRO 0
+		TexturedModel quadro0 = new TexturedModel(model4,new ModelTexture(loader.loadTexture("quadro0")));	
+		Entity entity4 = new Entity(quadro0, new Vector3f(primeiraParede,alturaQuadros,esquerdaParede),0,0,0,1);
+		entity4.increaseRotation(0,-180, 0);
 	
-		entity4.increaseRotation(0,-90, 0);
-	
+		//QUADRO 1	
+		TexturedModel quadro1 = new TexturedModel(model4,new ModelTexture(loader.loadTexture("quadro1")));	
+		Entity entity5 = new Entity(quadro1, new Vector3f(primeiraParede,alturaQuadros,direitaParede),0,0,0,1);
+		entity5.increaseRotation(0,-180, 0);
 		
+		//QUADRO 2
+		TexturedModel quadro2 = new TexturedModel(model4,new ModelTexture(loader.loadTexture("quadro2")));	
+		Entity entity6 = new Entity(quadro2, new Vector3f(segundaParede,alturaQuadros,esquerdaParede),0,0,0,1);
+		entity6.increaseRotation(0,-180, 0);
+
+		//QUADRO 3
+		TexturedModel quadro3 = new TexturedModel(model4,new ModelTexture(loader.loadTexture("quadro3")));	
+		Entity entity7 = new Entity(quadro3, new Vector3f(segundaParede,alturaQuadros,direitaParede),0,0,0,1);
+		entity7.increaseRotation(0,-180, 0);
+		
+		//QUADRO 4
+		TexturedModel quadro4 = new TexturedModel(model4,new ModelTexture(loader.loadTexture("quadro4")));	
+		Entity entity8 = new Entity(quadro4, new Vector3f(terceiraParede,alturaQuadros,esquerdaParede),0,0,0,1);
+		entity8.increaseRotation(0,-180, 0);
+			
+		//QUADRO 5
+		TexturedModel quadro5 = new TexturedModel(model4,new ModelTexture(loader.loadTexture("quadro5")));	
+		Entity entity9 = new Entity(quadro5, new Vector3f(terceiraParede,alturaQuadros,direitaParede),0,0,0,1);
+		entity9.increaseRotation(0,-180, 0);
+		
+		//QUADRO 6
+		TexturedModel quadro6 = new TexturedModel(model4,new ModelTexture(loader.loadTexture("quadro6")));	
+		Entity entity10 = new Entity(quadro6, new Vector3f(quartaParede,alturaQuadros,esquerdaParede),0,0,0,1);
+		entity10.increaseRotation(0,-180, 0);
+			
+		
+		//QUADRO 7
+		TexturedModel quadro7 = new TexturedModel(model4,new ModelTexture(loader.loadTexture("quadro7")));
+		Entity entity11 = new Entity(quadro7, new Vector3f(quartaParede,alturaQuadros,direitaParede),0,0,0,1);
+		entity11.increaseRotation(0,-180, 0);										
+				
+		
+			
+		//construindo a camera;
 		Camera camera = new Camera();
+		
 		
 		while(!Display.isCloseRequested()){
 			entity.increaseRotation(0,0, 0);
@@ -152,7 +126,21 @@ public class MainGameLoop {
 			renderer.render(entity,shader);
 			renderer.render(entity2,shader);
 			renderer.render(entity3,shader);
+			
+			/*quadros*/
 			renderer.render(entity4,shader);
+			renderer.render(entity5, shader);
+			renderer.render(entity6, shader);
+			renderer.render(entity7, shader);
+			renderer.render(entity8, shader);
+			renderer.render(entity9, shader);
+			renderer.render(entity10, shader);
+			renderer.render(entity11, shader);
+			
+			
+			
+			/*/quadros*/
+			
 			
 			shader.stop();
 			DisplayManager.updateDisplay();
--- src/entities/Camera.java
+++ src/entities/Camera.java
@@ -1,9 +1,18 @@
 package entities;
 
 import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
 import org.lwjgl.util.vector.Vector3f;
 
 public class Camera {
+	
+	
+	private static final float RUN_SPEED = 20;
+	private static final float TURN_SPEED = 160;
+	
+	private float currentSpeed = 0;
+	private float currentTurnSpeed = 0;	
+	private float angleAroundPlayer = 0;
 	
 	private Vector3f position = new Vector3f(0,0,0);
 	private float pitch;
@@ -13,17 +22,37 @@ public class Camera {
 	public Camera(){}
 	
 	public void move(){
+		
+		//calculateZoom();
+		//float horizontalDistance = calculateHorizontalDistance();
+		//float verticalDistance = calculateVerticalDistance();
+		//calculateCameraPosition(horizontalDistance, verticalDistance);
+		
+		//calcula o angulo da câmera em relação ao chão e a rotação na horizontal;
+		calculatePitch();
+		calculateAngleAroundPlayer();
+		this.yaw = 180 - angleAroundPlayer;
+		
+		//movimentação pelo teclado;
+		checkInputs();
+		
+		
 		if(Keyboard.isKeyDown(Keyboard.KEY_W)){
-			position.z-=0.02f;
+			position.z -= 0.05f;
 		}
 		if(Keyboard.isKeyDown(Keyboard.KEY_D)){
-			position.x+=0.02f;
+			position.x+=0.05f;
 		}
 		if(Keyboard.isKeyDown(Keyboard.KEY_A)){
-			position.x-=0.02f;
+			position.x-=0.05f;
 		}
+		if(Keyboard.isKeyDown(Keyboard.KEY_S)){
+			position.z+=0.05f;
+		}
+		
+		
 	}
-
+	
 	public Vector3f getPosition() {
 		return position;
 	}
@@ -41,5 +70,70 @@ public class Camera {
 	}
 	
 	
+	/**	(caso exista um personagem);
+	private void calculateCameraPosition(float horizontalDistance, float verticalDistance) {
+		float offsetX = (float) (horizontalDistance * Math.sin(Math.toRadians(angleAroundPlayer)));
+		float offsetZ = (float) (horizontalDistance * Math.cos(Math.toRadians(angleAroundPlayer)));
+		position.x = position.getX() - offsetX; 
+		position.y = position.getY() + verticalDistance; 
+		position.z = position.getZ() - offsetZ; 
+	}
+	
+	private float calculateHorizontalDistance() {
+		return (float) (distanceFromPlayer * Math.cos(Math.toRadians(pitch)));
+	}
+	
+	private float calculateVerticalDistance() {
+		return (float) (distanceFromPlayer * Math.sin(Math.toRadians(pitch)));
+	}
+	**/
+	
+	
+	/**
+	 * aplica zoom na tela; (no caso do nosso projeto nao será utilizado);
+	 */
+	private void calculateZoom() {
+		float zoomLevel = Mouse.getDWheel() * 0.1f;
+	}
+	
+	/**
+	 * Calcula o angulo que a camera está em relação do solo,
+	 * permitindo mover a camera para cima e para baixo com o click Direito + movimentação do mouse;
+	 */
+	private void calculatePitch() {
+		if(Mouse.isButtonDown(1)) {
+			float pitchChange = Mouse.getDY() * 0.1f;
+			pitch -= pitchChange;
+		}
+	}
+	
+	/**
+	 * Calcula o Angulo na horizontal,
+	 * permitindo girar a câmera para direita e esquerda com o click+movimentação do mouse;
+	 */
+	private void calculateAngleAroundPlayer() {
+		if(Mouse.isButtonDown(0)) {
+			float angleChange = Mouse.getDX() * 0.3f;
+			angleAroundPlayer -= angleChange;
+		}
+	}
+	
+	public void checkInputs() {
+		if(Keyboard.isKeyDown(Keyboard.KEY_W)) {
+			this.currentSpeed = RUN_SPEED;
+		}else if(Keyboard.isKeyDown(Keyboard.KEY_S)) {
+			this.currentSpeed = -RUN_SPEED;
+		}else {
+			this.currentSpeed = 0;
+		}
+		
+		if(Keyboard.isKeyDown(Keyboard.KEY_A)) {
+			this.currentTurnSpeed = TURN_SPEED;
+		}else if(Keyboard.isKeyDown(Keyboard.KEY_D)) {
+			this.currentTurnSpeed = -TURN_SPEED;
+		}else {
+			this.currentTurnSpeed = 0;
+		}
+	}
 
 }
